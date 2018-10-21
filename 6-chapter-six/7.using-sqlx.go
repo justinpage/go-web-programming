a3e6b2fbff2c0901b7a61b68b68ba982cee7746e
Justin Page <justin.page@motortrend.com>
2018-10-21 13:49:25 -0700

    Using sqlx and gorm

--- 6-chapter-six/7.using-sqlx.go
+++ 6-chapter-six/7.using-sqlx.go
@@ -1,32 +1,23 @@
 package main
 
 import (
-	"database/sql"
-	"errors"
 	"fmt"
 
+	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
 
 type Post struct {
-	Id       int
-	Content  string
-	Author   string
-	Comments []Comment
+	Id         int
+	Content    string
+	AuthorName string `db: author`
 }
 
-type Comment struct {
-	Id      int
-	Content string
-	Author  string
-	Post    *Post
-}
-
-var Db *sql.DB
+var Db *sqlx.DB
 
 func init() {
 	var err error
-	Db, err = sql.Open(
+	Db, err = sqlx.Open(
 		"postgres", "user=postgres dbname=postgres sslmode=disable",
 	)
 	if err != nil {
@@ -34,61 +25,27 @@ func init() {
 	}
 }
 
-func (comment *Comment) Create() (err error) {
-	if comment.Post == nil {
-		err = errors.New("Post not found")
-		return
-	}
-	err = Db.QueryRow(
-		"insert into comments (content, author, post_id) values ($1, $2, $3) returning id",
-		comment.Content, comment.Author, comment.Post.Id,
-	).Scan(&comment.Id)
-	return
-
-}
-
 func GetPost(id int) (post Post, err error) {
 	post = Post{}
-	post.Comments = []Comment{}
-	err = Db.QueryRow(
+	err = Db.QueryRowx(
 		"select id, content, author from posts where id = $1", id,
-	).Scan(&post.Id, &post.Content, &post.Author)
-
-	rows, err := Db.Query(
-		"select id, content, author from comments where post_id = $1", id,
-	)
+	).StructScan(&post)
 	if err != nil {
 		return
 	}
-	for rows.Next() {
-		comment := Comment{Post: &post}
-		err = rows.Scan(&comment.Id, &comment.Content, &comment.Author)
-		if err != nil {
-			return
-		}
-		post.Comments = append(post.Comments, comment)
-	}
-	rows.Close()
 	return
 }
 
 func (post *Post) Create() (err error) {
 	err = Db.QueryRow(
 		"insert into posts (content, author) values ($1, $2) returning id",
-		post.Content, post.Author,
+		post.Content, post.AuthorName,
 	).Scan(&post.Id)
 	return
 }
 
 func main() {
-	post := Post{Content: "Hello World!", Author: "Justin Page"}
+	post := Post{Content: "Hello World!", AuthorName: "Justin Page"}
 	post.Create()
-
-	comment := Comment{Content: "Good post!", Author: "Joe", Post: &post}
-	comment.Create()
-	readPost, _ := GetPost(post.Id)
-
-	fmt.Println(readPost)
-	fmt.Println(readPost.Comments)
-	fmt.Println(readPost.Comments[0].Post)
+	fmt.Println(post)
 }
