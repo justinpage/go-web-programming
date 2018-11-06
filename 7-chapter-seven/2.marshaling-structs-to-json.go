3ce7d2dfee7c945ed52da3973142ab1868bd22f1
Justin Page <justin.page@motortrend.com>
2018-11-05 18:04:06 -0800

    Creating and marshaling structs to json

---
+++ 7-chapter-seven/2.marshaling-structs-to-json.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+type Post struct {
+	Id       int       `json:"id"`
+	Content  string    `json:"content"`
+	Author   Author    `json:"author"`
+	Comments []Comment `json:"comments"`
+}
+
+type Author struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type Comment struct {
+	Id      int    `json:"id"`
+	Content string `json:"content"`
+	Author  string `json:"author"`
+}
+
+func main() {
+	post := Post{
+		Id:      1,
+		Content: "Hello World!",
+		Author: Author{
+			Id:   2,
+			Name: "Justin Page",
+		},
+		Comments: []Comment{
+			Comment{
+				Id:      3,
+				Content: "Have a great day!",
+				Author:  "Adam",
+			},
+			Comment{
+				Id:      4,
+				Content: "How are you today?",
+				Author:  "Betty",
+			},
+		},
+	}
+
+	output, err := json.MarshalIndent(&post, "", "\t")
+	if err != nil {
+		fmt.Println("Error marshalling to JSON:", err)
+		return
+	}
+	err = ioutil.WriteFile("2.marshaling-structs-to.json", output, 0644)
+	if err != nil {
+		fmt.Println("Error writing JSON file:", err)
+		return
+	}
+}
